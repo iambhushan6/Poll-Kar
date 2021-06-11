97a50538ca0696b6dc5de423faed3356ddae5b27
iambhushan6 <bhushan.lokhande19@vit.edu>
2021-06-11 15:59:43 +0530

    static_root updated

--- pollkar/settings.py
+++ pollkar/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+STATIC_ROOT = "https://pollkar.herokuapp.com/static/"
+# STATIC_ROOT = "/var/www/example.com/static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
